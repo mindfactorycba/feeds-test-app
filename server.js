ff7984803b74a1a3ca5182a4652fe12d481da4c5
Agustin Miani <agustinmiani@gmail.com>
2023-06-21 15:28:25 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+
+app.post('/feeds', (req, res) => {
+  // Aquí puedes realizar cualquier lógica adicional con los datos recibidos en el cuerpo (req.body)
+
+  // Envía una respuesta exitosa con código de estado 200 y un cuerpo de éxito
+  res.status(200).json({ success: true });
+});
+
+app.get('/ping', (req, res) => {
+    // Aquí puedes realizar cualquier lógica adicional con los datos recibidos en el cuerpo (req.body)
+  
+    // Envía una respuesta exitosa con código de estado 200 y un cuerpo de éxito
+    res.status(200).json("pong");
+  });
+
+app.listen(port, () => {
+  console.log(`Servidor web escuchando en el puerto ${port}`);
+});
+
