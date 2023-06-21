62d842c4f2fd55e2710dbd8688e6c8ebe071babb
Agustin Miani <agustinmiani@gmail.com>
2023-06-21 17:39:44 -0300

    remove comments

--- server.js
+++ server.js
@@ -6,16 +6,10 @@ app.use(express.json());
 
 
 app.post('/feeds', (req, res) => {
-  // Aquí puedes realizar cualquier lógica adicional con los datos recibidos en el cuerpo (req.body)
-
-  // Envía una respuesta exitosa con código de estado 200 y un cuerpo de éxito
   res.status(200).json({ success: true });
 });
 
 app.get('/ping', (req, res) => {
-    // Aquí puedes realizar cualquier lógica adicional con los datos recibidos en el cuerpo (req.body)
-  
-    // Envía una respuesta exitosa con código de estado 200 y un cuerpo de éxito
     res.status(200).json("pong");
   });
 
