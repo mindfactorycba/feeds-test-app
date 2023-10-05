45abfbd64c3b9333279148dad5a9025678c51a0c
mindfactory <55879594+mindfactorycba@users.noreply.github.com>
2023-10-05 13:09:28 -0300

    return error

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 app.post('/feeds', (req, res) => {
   console.log("El body que recibimos fue:" + JSON.stringify(req.body));
-  res.status(200).json({ success: true });
-  // res.status(500).json({ fail: true });
+  //res.status(200).json({ success: true });
+  res.status(500).json({ fail: true });
 });
 
 app.get('/ping', (req, res) => {
